54223e3783d68b7cbd885d14a9d3b1fbe4c4a6e9
djm204 <desiratech@gmail.com>
2014-03-30 20:18:05 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+UnderPantsGnomes::Application.config.secret_key_base = 'f1932f7109f0e4f386fca55c6b27b570d9e76945bedabade2b2fa2739e2c012f3a5568a5a483666045017a94d6321dcfcfd79f6b8d147e85e3e6ec1f19c8131b'
